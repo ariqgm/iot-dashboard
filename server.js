0d4f36e99eff65f00c8019a787e16409076675de
ariq <ariq@gmail.com>
2024-09-07 12:16:34 +0700

    update

--- server.js
+++ server.js
@@ -39,7 +39,7 @@
   let lastDimmerValue = 0; // Inisialisasi nilai dimmer terakhir
   let lampStatus = false;
   // Konfigurasi MQTT
-  const mqttClient = mqtt.connect('http://3.0.182.144:1883', {
+  const mqttClient = mqtt.connect('http://54.179.173.181:1883', {
     username: 'ariq',
     password: '1234'
   });
